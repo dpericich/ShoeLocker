5e1d4ac08580f7dd70ba4546425e219a60e3c8d6
Daniel <dpericich@gmail.com>
2021-02-10 12:34:04 -0600

    added product pages with show method from products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/welcome', to: "welcome#index"
+  root "products#index"
   
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
