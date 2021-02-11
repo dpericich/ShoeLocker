2e1f623ad0814b241dfa5e7cf1a307e287e890b7
Daniel <dpericich@gmail.com>
2021-02-10 21:59:41 -0600

    added ability to delete products and styled product pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,28 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end 
 
+  def new
+    @product = Product.new
+  end 
+
+  def create 
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else  
+      render root
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.delete
+    redirect_to root_path
+  end
+
   private 
   def product_params
-    params.require(:product).permit(:title, :description, :gender, :price)
+    params.permit(:title, :description, :gender, :price)
   end 
 end
